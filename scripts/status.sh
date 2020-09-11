dw-0/kiauh

commit c98584dde8472421101247a97cae862dd1dfdb85

bugfix: kiauh status check prevents kiauh from starting if no kiauh .git directory present

scripts/status.sh
@@ -1,15 +1,17 @@
 kiauh_status(){
-  cd ${HOME}/kiauh
-  git fetch --all -q
-  if git branch -a | grep "* master" -q; then
-    CURR_KIAUH_BRANCH="master" #needed to display branch in UI
-    if [[ "$(git rev-parse --short=8 origin/master)" != "$(git rev-parse --short=8 HEAD)" ]]; then
-      KIAUH_UPDATE_AVAIL="true"
-    fi
-  elif git branch -a | grep "* dev-2.0" -q; then
-    CURR_KIAUH_BRANCH="dev-2.0" #needed to display branch in UI
-    if [[ "$(git rev-parse --short=8 origin/dev-2.0)" != "$(git rev-parse --short=8 HEAD)" ]]; then
-      KIAUH_UPDATE_AVAIL="true"
+  if [ -d {$HOME}/kiauh/.git ]; then
+    cd ${HOME}/kiauh
+    git fetch --all -q
+    if git branch -a | grep "* master" -q; then
+      CURR_KIAUH_BRANCH="master" #needed to display branch in UI
+      if [[ "$(git rev-parse --short=8 origin/master)" != "$(git rev-parse --short=8 HEAD)" ]]; then
+        KIAUH_UPDATE_AVAIL="true"
+      fi
+    elif git branch -a | grep "* dev-2.0" -q; then
+      CURR_KIAUH_BRANCH="dev-2.0" #needed to display branch in UI
+      if [[ "$(git rev-parse --short=8 origin/dev-2.0)" != "$(git rev-parse --short=8 HEAD)" ]]; then
+        KIAUH_UPDATE_AVAIL="true"
+      fi
     fi
   fi
 }

commit 0b474514b1a67c4c7ce97f02638f171abd0608cc

fix: bug introduced with c98584d

scripts/status.sh
@@ -1,5 +1,5 @@
 kiauh_status(){
-  if [ -d {$HOME}/kiauh/.git ]; then
+  if [ -d "${HOME}/kiauh/.git" ]; then
     cd ${HOME}/kiauh
     git fetch --all -q
     if git branch -a | grep "* master" -q; then