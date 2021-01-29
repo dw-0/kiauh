17524bf49b534558f85bf589c7052bbac91e7846
th33xitus <th33xitus@googlemail.com>
2021-01-29 20:10:04 +0100

    fix: lock changing config folder function until klipper was installed through kiauh.

--- scripts/ui/settings_menu.sh
+++ scripts/ui/settings_menu.sh
@@ -1,6 +1,5 @@
 settings_ui(){
   source_kiauh_ini
-  [ -z $klipper_cfg_loc ] && klipper_cfg_loc="----------"
   top_border
   echo -e "|     $(title_msg "~~~~~~~~~~~~ [ KIAUH Settings ] ~~~~~~~~~~~~~")     | "
   hr
@@ -16,31 +15,29 @@ settings_ui(){
   printf "|%-55s|\n" "    $klipper_cfg_loc"
   blank_line
   hr
+  if [ -z $klipper_cfg_loc ]; then
+  echo -e "|  ${red}N/A) Install Klipper with KIAUH first to unlock!${default}     | "
+  else
   echo -e "|  1) Change config folder                              | "
+  fi
   quit_footer
 }
 
 settings_menu(){
-  print_header
-  print_msg && clear_msg
-  settings_ui
+  do_action "" "settings_ui"
   while true; do
     read -p "${cyan}Perform action:${default} " action; echo
     case "$action" in
       1)
-        clear
-        print_header
-        change_klipper_cfg_path
-        print_msg && clear_msg
-        settings_ui;;
+        if [ ! -z $klipper_cfg_loc ]; then
+          do_action "change_klipper_cfg_path" "settings_ui"
+        else
+          deny_action "settings_ui"
+        fi;;
       Q|q)
         clear; main_menu; break;;
       *)
-        clear
-        print_header
-        print_unkown_cmd
-        print_msg && clear_msg
-        settings_ui;;
+        deny_action "settings_ui";;
     esac
   done
   settings_ui
