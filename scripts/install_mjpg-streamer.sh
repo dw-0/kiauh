8abda56749154b3acf62d145b1938a24a38fc0f9
th33xitus <th33xitus@googlemail.com>
2021-05-25 12:04:13 +0200

    fix: typo in WEBCAM_TXT_SRC

--- scripts/install_mjpg-streamer.sh
+++ scripts/install_mjpg-streamer.sh
@@ -1,7 +1,7 @@
 ### base variables
 SYSTEMDDIR="/etc/systemd/system"
 WEBCAMD_SRC="https://raw.githubusercontent.com/raymondh2/MainsailOS/master/src/modules/mjpgstreamer/filesystem/root/usr/local/bin/webcamd"
-WEBCAM_TXT_SRC="https://raw.githubusercontent.com/raymondh2/MainsailOS/master/src/modules/mjpgstreamer/filesystem/home/pi/klipper-config/webcam.txt"
+WEBCAM_TXT_SRC="https://raw.githubusercontent.com/raymondh2/MainsailOS/master/src/modules/mjpgstreamer/filesystem/home/pi/klipper_config/webcam.txt"
 
 install_mjpg-streamer(){
   ### checking dependencies
