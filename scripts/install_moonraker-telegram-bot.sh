4bd0796d5488420f84931cc7f17fbfb8e30b5208
Evgenii <shavrin0591@gmail.com>
2021-09-19 17:35:15 +0300

    change installation

--- scripts/install_moonraker-telegram-bot.sh
+++ scripts/install_moonraker-telegram-bot.sh
@@ -5,42 +5,8 @@ install_MoonrakerTelegramBot(){
     get_user_selections_MoonrakerTelegramBot
     #MoonrakerTelegramBot main installation
     MoonrakerTelegramBot_setup
-    #execute customizations
-    symlinks_MoonrakerTelegramBot
     #after install actions
     restart_MoonrakerTelegramBot
-}
-
-system_check_MoonrakerTelegramBot(){
-  source_kiauh_ini
-  if [ ! -e $klipper_cfg_loc/telegram.log ]; then
-    MOONRAKERTELEGRAMBOT_SL_FOUND="false"
-  else
-    MOONRAKERTELEGRAMBOT_SL_FOUND="true"
-  fi
-}
-
-get_user_selections_MoonrakerTelegramBot(){
-  #user selection for telegram.log symlink
-  if [ "$KMOONRAKERTELEGRAMBOT_SL_FOUND" = "false" ]; then
-    while true; do
-      echo
-      read -p "${cyan}###### Create telegram.log symlink? (y/N):${default} " yn
-      case "$yn" in
-        Y|y|Yes|yes)
-          echo -e "###### > Yes"
-          SEL_MTBLOG_SL="true"
-          break;;
-        N|n|No|no|"")
-          echo -e "###### > No"
-          SEL_MTBLOG_SL="false"
-          break;;
-        *)
-          print_unkown_cmd
-          print_msg && clear_msg;;
-      esac
-    done
-  fi
 }
 
 MoonrakerTelegramBot_setup(){
@@ -53,7 +19,7 @@ MoonrakerTelegramBot_setup(){
   cd ${HOME} && git clone $NLEF_REPO
   ok_msg "Download complete!"
   status_msg "Installing MoonrakerTelegramBot ..."
-  $MOONRAKERTELEGRAMBOT_DIR/scripts/install.sh
+  $MOONRAKERTELEGRAMBOT_DIR/scripts/install.sh 
   echo; ok_msg "MoonrakerTelegramBot successfully installed!"
 }
 
