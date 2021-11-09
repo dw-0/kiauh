7ca1ccbe93682bf4ce4da2dcdc9aa49c25b8655c
Yifei Ding <dingyifeiair@gmail.com>
2021-11-09 11:41:56 -0800

    scripts: switch print_msg and main_menu to whiptail

--- scripts/ui/main_menu.sh
+++ scripts/ui/main_menu.sh
@@ -1,22 +1,22 @@
-main_ui(){
+#main_ui(){
   #[ $KIAUH_UPDATE_REMIND="true" ] && kiauh_update_reminder
-  top_border
-  echo -e "|     $(title_msg "~~~~~~~~~~~~~~~ [ Main Menu ] ~~~~~~~~~~~~~~~")     |"
-  hr
-  echo -e "|  0) [Upload Log]     |       Klipper: $KLIPPER_STATUS|"
-  echo -e "|                      |        Branch: ${cyan}$PRINT_BRANCH${default}|"
-  echo -e "|  1) [Install]        |                                |"
-  echo -e "|  2) [Update]         |     Moonraker: $MOONRAKER_STATUS|"
-  echo -e "|  3) [Remove]         |                                |"
-  echo -e "|  4) [Advanced]       |      Mainsail: $MAINSAIL_STATUS|"
-  echo -e "|  5) [Backup]         |        Fluidd: $FLUIDD_STATUS|"
-  echo -e "|                      | KlipperScreen: $KLIPPERSCREEN_STATUS|"
-  echo -e "|  6) [Settings]       |  Telegram Bot: $MOONRAKER_TELEGRAM_BOT_STATUS|"
-  echo -e "|                      |                                |"
-  echo -e "|                      |          DWC2: $DWC2_STATUS|"
-  echo -e "|  ${cyan}$KIAUH_VER${default}|     Octoprint: $OCTOPRINT_STATUS|"
-  quit_footer
-}
+#  top_border
+#  echo -e "|     $(title_msg "~~~~~~~~~~~~~~~ [ Main Menu ] ~~~~~~~~~~~~~~~")     |"
+#  hr
+#  echo -e "|  0) [Upload Log]     |       Klipper: $KLIPPER_STATUS|"
+#  echo -e "|                      |        Branch: ${cyan}$PRINT_BRANCH${default}|"
+#  echo -e "|  1) [Install]        |                                |"
+#  echo -e "|  2) [Update]         |     Moonraker: $MOONRAKER_STATUS|"
+#  echo -e "|  3) [Remove]         |                                |"
+#  echo -e "|  4) [Advanced]       |      Mainsail: $MAINSAIL_STATUS|"
+#  echo -e "|  5) [Backup]         |        Fluidd: $FLUIDD_STATUS|"
+#  echo -e "|                      | KlipperScreen: $KLIPPERSCREEN_STATUS|"
+#  echo -e "|  6) [Settings]       |  Telegram Bot: $MOONRAKER_TELEGRAM_BOT_STATUS|"
+#  echo -e "|                      |                                |"
+#  echo -e "|                      |          DWC2: $DWC2_STATUS|"
+#  echo -e "|  ${cyan}$KIAUH_VER${default}|     Octoprint: $OCTOPRINT_STATUS|"
+#  quit_footer
+#}
 
 print_kiauh_version(){
   cd ${SRCDIR}/kiauh
@@ -40,7 +40,7 @@ kiauh_update_dialog(){
 }
 
 main_menu(){
-  print_header
+#  print_header
   #print KIAUH update msg if update available
     if [ "$KIAUH_UPDATE_AVAIL" = "true" ]; then
       kiauh_update_dialog
@@ -57,36 +57,60 @@ main_menu(){
     MoonrakerTelegramBot_status
     print_branch
   print_msg && clear_msg
-  main_ui
+
   while true; do
-    read -p "${cyan}Perform action:${default} " action; echo
-    case "$action" in
-      "start klipper") do_action_service "start" "klipper"; main_ui;;
-      "stop klipper") do_action_service "stop" "klipper"; main_ui;;
-      "restart klipper") do_action_service "restart" "klipper"; main_ui;;
-      "start moonraker") do_action_service "start" "moonraker"; main_ui;;
-      "stop moonraker") do_action_service "stop" "moonraker"; main_ui;;
-      "restart moonraker")do_action_service "restart" "moonraker"; main_ui;;
-      "start dwc") do_action_service "start" "dwc"; main_ui;;
-      "stop dwc") do_action_service "stop" "dwc"; main_ui;;
-      "restart dwc") do_action_service "restart" "dwc"; main_ui;;
-      "start octoprint") do_action_service "start" "octoprint"; main_ui;;
-      "stop octoprint") do_action_service "stop" "octoprint"; main_ui;;
-      "restart octoprint") do_action_service "restart" "octoprint"; main_ui;;
-      update) do_action "update_kiauh" "main_ui";;
-      0) do_action "upload_selection" "main_ui";;
-      1) clear && install_menu && break;;
-      2) clear && update_menu && break;;
-      3) clear && remove_menu && break;;
-      4) clear && advanced_menu && break;;
-      5) clear && backup_menu && break;;
-      6) clear && settings_menu && break;;
-      Q|q)
-        echo -e "${green}###### Happy printing! ######${default}"; echo
-        exit -1;;
-      *)
-        deny_action "main_ui";;
+      MAIN_MENU=$(whiptail --title "$KIAUH_TITLE" --cancel-button "Quit" --menu "Choose an option:"\
+    "$KIAUH_WHIPTAIL_NORMAL_HEIGHT" "$KIAUH_WHIPTAIL_NORMAL_WIDTH" 8\
+    "0 Upload Log" "Upload Log For Bug Report"\
+    "1 Status" "Status Page"\
+    "2 Install" "Install Instances"\
+    "3 Update" "Update Instances"\
+    "4 Remove" "Remove Instances"\
+    "5 Advanced" "Advanced Settings"\
+    "6 Backup" "Backup"\
+    "7 Settings" "Configure KIAUH" 3>&1 1>&2 2>&3)
+    case "$MAIN_MENU" in
+      1\ *) echo "Current status" ;;
+      2\ *) install_menu ;;
+      3\ *) remove_menu ;;
+      4\ *) advanced_menu ;;
+      5\ *) backup_menu ;;
+      6\ *) settings_menu ;;
+      7\ *) settings_menu ;;
+      0\ *) upload_selection;;
+      *) echo "Unknown Command";;
     esac
   done
-  clear; main_menu
+
+#  while true; do
+#    read -p "${cyan}Perform action:${default} " action; echo
+#    case "$action" in
+#      "start klipper") do_action_service "start" "klipper"; main_ui;;
+#      "stop klipper") do_action_service "stop" "klipper"; main_ui;;
+#      "restart klipper") do_action_service "restart" "klipper"; main_ui;;
+#      "start moonraker") do_action_service "start" "moonraker"; main_ui;;
+#      "stop moonraker") do_action_service "stop" "moonraker"; main_ui;;
+#      "restart moonraker")do_action_service "restart" "moonraker"; main_ui;;
+#      "start dwc") do_action_service "start" "dwc"; main_ui;;
+#      "stop dwc") do_action_service "stop" "dwc"; main_ui;;
+#      "restart dwc") do_action_service "restart" "dwc"; main_ui;;
+#      "start octoprint") do_action_service "start" "octoprint"; main_ui;;
+#      "stop octoprint") do_action_service "stop" "octoprint"; main_ui;;
+#      "restart octoprint") do_action_service "restart" "octoprint"; main_ui;;
+#      update) do_action "update_kiauh" "main_ui";;
+#      0) do_action "upload_selection" "main_ui";;
+#      1) clear && install_menu && break;;
+#      2) clear && update_menu && break;;
+#      3) clear && remove_menu && break;;
+#      4) clear && advanced_menu && break;;
+#      5) clear && backup_menu && break;;
+#      6) clear && settings_menu && break;;
+#      Q|q)
+#        echo -e "${green}###### Happy printing! ######${default}"; echo
+#        exit -1;;
+#      *)
+#        deny_action "main_ui";;
+#    esac
+#  done
+#  clear; main_menu
 }
