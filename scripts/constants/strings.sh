f5a7b2d1c5a252d90405ace35ddd88b7516ee3f6
Yifei Ding <dingyifeiair@gmail.com>
2021-12-30 21:47:49 -0800

    constants: Update

--- scripts/constants/strings.sh
+++ scripts/constants/strings.sh
@@ -1,16 +1,24 @@
-# shellcheck shell=bash
-
 KIAUH_TITLE="KIAUH - Klipper Installation And Update Helper"
-#names(They should never be the same because they are used in whiptail menu)
-readonly KLIPPER="Klipper"
-readonly MOONRAKER="Moonraker"
-readonly MAINSAIL="Mainsail"
-readonly FLUIDD="Fluidd"
-readonly KLIPPERSCREEN="KlipperScreen"
-readonly DWC2FK="DWC-for-Klipper"
-readonly DWC2="DWC2 Web UI"
-readonly DWC="Duet Web Control" #Addressing both DWC2 and DWC2FK
-readonly PGC="PrettyGCode"
-readonly MOONRAKER_TELEGRAM_BOT="Telegram Bot"
-readonly SYSTEM="System"
-readonly OCTOPRINT="OctoPrint"
+
+#names
+declare -A READABLE_NAMES
+READABLE_NAMES["$KLIPPER"]="Klipper"
+READABLE_NAMES["$MOONRAKER"]="Moonraker"
+READABLE_NAMES["$MAINSAIL"]="Mainsail"
+READABLE_NAMES["$FLUIDD"]="Fluidd"
+READABLE_NAMES["$KLIPPERSCREEN"]="KlipperScreen"
+READABLE_NAMES["$DWC2FK"]="DWC-for-Klipper"
+READABLE_NAMES["$DWC2"]="DWC2 Web UI"
+READABLE_NAMES["$DWC"]="Duet Web Control" #Addressing DWC2 and DWC2FK
+READABLE_NAMES["$PGC"]="PrettyGCode"
+READABLE_NAMES["$MOONRAKER_TELEGRAM_BOT"]="Telegram Bot"
+READABLE_NAMES["$SYSTEM"]="System"
+READABLE_NAMES["$OCTOPRINT"]="OctoPrint"
+READABLE_NAMES["$MJPG_STREAMER"]="Webcam MJPG-Streamer"
+READABLE_NAMES["$NGINX"]="Nginx"
+readonly NOT_INSTALLED="Not installed!"
+readonly INCOMPLETE="Incomplete!"
+readonly INSTALLED="Installed!"
+
+#display this as placeholder if no version/commit could be fetched
+readonly NONE="None"
