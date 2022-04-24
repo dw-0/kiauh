fe393f819620675c92eabc7e69c456329d1f8d1f
th33xitus <th33xitus@googlemail.com>
2022-04-24 14:24:45 +0200

    refactor: add copyright notice and shebang to upload_log.sh
    Signed-off-by: Dominik Willner th33xitus@gmail.com

--- scripts/upload_log.sh
+++ scripts/upload_log.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+#=======================================================================#
+# Copyright (C) 2020 - 2022 Dominik Willner <th33xitus@gmail.com>       #
+#                                                                       #
+# This file is part of KIAUH - Klipper Installation And Update Helper   #
+# https://github.com/th33xitus/kiauh                                    #
+#                                                                       #
+# This file may be distributed under the terms of the GNU GPLv3 license #
+#=======================================================================#
+
+set -e
+
 function accept_upload_conditions(){
   top_border
   echo -e "|     ${red}~~~~~~~~~~~ [ Upload Agreement ] ~~~~~~~~~~~~${white}     |"
