cb3661b8b56c09cb7277148a84b53fd628f185e9
th33xitus <th33xitus@googlemail.com>
2022-10-25 15:23:57 +0200

    refactor(prettygcode): re-enable pretty gcode installer
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- scripts/ui/install_menu.sh
+++ scripts/ui/install_menu.sh
@@ -62,10 +62,7 @@ function install_menu() {
       6)
         do_action "octoprint_setup_dialog" "install_ui";;
       7)
-        #do_action "install_pgc_for_klipper" "install_ui";;
-        clear && print_header
-        print_error "Function currently disabled! Sorry!"
-        install_ui;;
+        do_action "install_pgc_for_klipper" "install_ui";;
       8)
         #do_action "telegram_bot_setup_dialog" "install_ui";;
         clear && print_header
