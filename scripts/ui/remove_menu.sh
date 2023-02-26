fd27db28d46ca5ff2f76a234ba0e9c429198dc3f
th33xitus <th33xitus@googlemail.com>
2023-02-26 16:29:01 +0100

    feat(mainsail): use mainsail-config repo for downloading mainsail macros (#301)

--- scripts/ui/remove_menu.sh
+++ scripts/ui/remove_menu.sh
@@ -18,17 +18,17 @@ function remove_ui() {
   echo -e "| ${yellow}INFO: Configurations and/or any backups will be kept!${white} |"
   hr
   echo -e "| Firmware & API:           | 3rd Party Webinterface:   |"
-  echo -e "|  1) [Klipper]             |  6) [OctoPrint]           |"
+  echo -e "|  1) [Klipper]             |  7) [OctoPrint]           |"
   echo -e "|  2) [Moonraker]           |                           |"
   echo -e "|                           | Webcam Streamer:          |"
-  echo -e "| Klipper Webinterface:     |  7) [Crowsnest]           |"
-  echo -e "|  3) [Mainsail]            |  8) [MJPG-Streamer]       |"
-  echo -e "|  4) [Fluidd]              |                           |"
-  echo -e "|                           | Other:                    |"
-  echo -e "| Touchscreen GUI:          |  9) [PrettyGCode]         |"
-  echo -e "|  5) [KlipperScreen]       | 10) [Telegram Bot]        |"
-  echo -e "|                           | 11) [Obico for Klipper]   |"
-  echo -e "|                           | 12) [NGINX]               |"
+  echo -e "| Klipper Webinterface:     |  8) [Crowsnest]           |"
+  echo -e "|  3) [Mainsail]            |  9) [MJPG-Streamer]       |"
+  echo -e "|  4) [Mainsail-Config]     |                           |"
+  echo -e "|  5) [Fluidd]              | Other:                    |"
+  echo -e "|                           | 10) [PrettyGCode]         |"
+  echo -e "| Touchscreen GUI:          | 11) [Telegram Bot]        |"
+  echo -e "|  6) [KlipperScreen]       | 12) [Obico for Klipper]   |"
+  echo -e "|                           | 13) [NGINX]               |"
   back_footer
 }
 
@@ -46,22 +46,24 @@ function remove_menu() {
       3)
         do_action "remove_mainsail" "remove_ui";;
       4)
-        do_action "remove_fluidd" "remove_ui";;
+        do_action "remove_mainsail_config" "remove_ui";;
       5)
-        do_action "remove_klipperscreen" "remove_ui";;
+        do_action "remove_fluidd" "remove_ui";;
       6)
-        do_action "remove_octoprint" "remove_ui";;
+        do_action "remove_klipperscreen" "remove_ui";;
       7)
-        do_action "remove_crowsnest" "remove_ui";;
+        do_action "remove_octoprint" "remove_ui";;
       8)
-        do_action "remove_mjpg-streamer" "remove_ui";;
+        do_action "remove_crowsnest" "remove_ui";;
       9)
-        do_action "remove_prettygcode" "remove_ui";;
+        do_action "remove_mjpg-streamer" "remove_ui";;
       10)
-        do_action "remove_telegram_bot" "remove_ui";;
+        do_action "remove_prettygcode" "remove_ui";;
       11)
-        do_action "remove_moonraker_obico" "remove_ui";;
+        do_action "remove_telegram_bot" "remove_ui";;
       12)
+        do_action "remove_moonraker_obico" "remove_ui";;
+      13)
         do_action "remove_nginx" "remove_ui";;
       B|b)
         clear; main_menu; break;;
