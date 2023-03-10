8526acf8b6c4fe0e22cd5b52b5b6c5f2153424f9
th33xitus <th33xitus@googlemail.com>
2023-03-10 20:06:06 +0100

    chore: update copyright notice
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- kiauh.sh
+++ kiauh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #=======================================================================#
-# Copyright (C) 2020 - 2022 Dominik Willner <th33xitus@gmail.com>       #
+# Copyright (C) 2020 - 2023 Dominik Willner <th33xitus@gmail.com>       #
 #                                                                       #
 # This file is part of KIAUH - Klipper Installation And Update Helper   #
 # https://github.com/th33xitus/kiauh                                    #
@@ -9,13 +9,8 @@
 # This file may be distributed under the terms of the GNU GPLv3 license #
 #=======================================================================#
 
-# TODO: mjpg-streamer
-# TODO: moonraker-telegram-bot
-# TODO: obico
-# TODO: pretty_gcode
 # TODO: upload_log
 # TODO: all backup functions
-# TODO: octoprint
 # TODO: doublecheck that nothing got missed!
 
 set -e
--- scripts/flash_klipper.sh
+++ scripts/flash_klipper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #=======================================================================#
-# Copyright (C) 2020 - 2022 Dominik Willner <th33xitus@gmail.com>       #
+# Copyright (C) 2020 - 2023 Dominik Willner <th33xitus@gmail.com>       #
 #                                                                       #
 # This file is part of KIAUH - Klipper Installation And Update Helper   #
 # https://github.com/th33xitus/kiauh                                    #
