c9699246bb82ee9f21366f6768aafe5088ffac82
Cameron Ryder <cameron@crontech.live>
2023-06-11 23:41:23 -0400

    fix: clarify check_euid message regarding credential prompts

--- scripts/utilities.sh
+++ scripts/utilities.sh
@@ -20,6 +20,8 @@ function check_euid() {
     echo -e "${red}"
     top_border
     echo -e "|       !!! THIS SCRIPT MUST NOT RUN AS ROOT !!!        |"
+    echo -e "|                                                       |"
+    echo -e "|        It will ask for credentials as needed.         |"
     bottom_border
     echo -e "${white}"
     exit 1
