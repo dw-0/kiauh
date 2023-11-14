279d000bb050b22755ffd89611ee5f2941d3ed5a
dw-0 <th33xitus@gmail.com>
2023-11-14 21:31:25 +0100

    refactor(kiauh): specify python3 in shebang
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- kiauh/core/menus/settings_menu.py
+++ kiauh/core/menus/settings_menu.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # ======================================================================= #
 #  Copyright (C) 2020 - 2023 Dominik Willner <th33xitus@gmail.com>        #
