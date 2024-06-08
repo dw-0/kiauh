24b106c31890bac1e79d9ede47ffad44fd387768
Alessandro Maggi <maggialessandro360@gmail.com>
2024-06-08 13:46:39 +0200

    feat(backup): add spoolman script

--- scripts/ui/backup_menu.sh
+++ scripts/ui/backup_menu.sh
@@ -17,15 +17,18 @@ function backup_ui() {
   hr
   echo -e "| ${yellow}INFO: Backups are located in '~/kiauh-backups'${white}        |"
   hr
-  echo -e "| Klipper & API:             | Touchscreen GUI:         |"
-  echo -e "|  1) [Klipper]              |  7) [KlipperScreen]      |"
+  echo -e "| Klipper & API:             | Spool Manager:           |"
+  echo -e "|  1) [Klipper]              |  8) [Spoolman]           |"
   echo -e "|  2) [Moonraker]            |                          |"
   echo -e "|  3) [Config Folder]        | 3rd Party Webinterface:  |"
-  echo -e "|  4) [Moonraker Database]   |  8) [OctoPrint]          |"
+  echo -e "|  4) [Moonraker Database]   |  9) [OctoPrint]          |"
   echo -e "|                            |                          |"
   echo -e "| Klipper Webinterface:      | Other:                   |"
-  echo -e "|  5) [Mainsail]             |  9) [Telegram Bot]       |"
-  echo -e "|  6) [Fluidd]               | 10) [OctoEverywhere]     |"
+  echo -e "|  5) [Mainsail]             | 10) [Telegram Bot]       |"
+  echo -e "|  6) [Fluidd]               | 11) [OctoEverywhere]     |"
+  echo -e "|                            |                          |"
+  echo -e "| Touchscreen GUI:           |                          |"
+  echo -e "|  7) [KlipperScreen]        |                          |"
   back_footer
 }
 
@@ -51,10 +54,12 @@ function backup_menu() {
       7)
         do_action "backup_klipperscreen" "backup_ui";;
       8)
-        do_action "backup_octoprint" "backup_ui";;
+        do_action "backup_spoolman" "backup_ui";;
       9)
-        do_action "backup_telegram_bot" "backup_ui";;
+        do_action "backup_octoprint" "backup_ui";;
       10)
+        do_action "backup_telegram_bot" "backup_ui";;
+      11)
         do_action "backup_octoeverywhere" "backup_ui";;
       B|b)
         clear; main_menu; break;;
