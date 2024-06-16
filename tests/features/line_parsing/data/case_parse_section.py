60f8aef69ba552ce8d31066d2e09829d1cca9df2
dw-0 <th33xitus@gmail.com>
2024-06-16 18:14:55 +0200

    Squashed 'kiauh/core/submodules/simple_config_parser/' content from commit 188dd1f
    git-subtree-dir: kiauh/core/submodules/simple_config_parser
    git-subtree-split: 188dd1ffd80bf72a2dc6075147ddc9339b059c4b

---
+++ tests/features/line_parsing/data/case_parse_section.py
@@ -0,0 +1,6 @@
+testcases = [
+    ("[test_section]", "test_section"),
+    ("[test_section two]", "test_section two"),
+    ("[section1] # inline comment", "section1"),
+    ("[section2] ; second comment", "section2"),
+]
