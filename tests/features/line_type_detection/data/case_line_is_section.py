a17171b88e982c8343761bf07c5a4373acdbd205
dw-0 <th33xitus@gmail.com>
2024-06-19 21:14:50 +0200

    Squashed 'kiauh/core/submodules/simple_config_parser/' changes from 2698f60..7aa6586
    7aa6586 fix: sections can have hyphens in their second word
    44cedf5 fix(tests): fix whitespaces in expected output
    
    git-subtree-dir: kiauh/core/submodules/simple_config_parser
    git-subtree-split: 7aa658654eeb08fd53831effbfba4503a61e0eff

--- tests/features/line_type_detection/data/case_line_is_section.py
+++ tests/features/line_type_detection/data/case_line_is_section.py
@@ -1,5 +1,6 @@
 testcases = [
     ("[example_section]", True),
+    ("[update_manager moonraker-obico]", True),
     ("[example_section two]", True),
     ("not_a_valid_section", False),
     ("section: invalid", False),
