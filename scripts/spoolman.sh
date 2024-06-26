7993b98ee1da89beb2ae60a53be2f7c6e775a53e
Alessandro Maggi <59124971+DicyRoll@users.noreply.github.com>
2024-06-26 07:04:09 +0200

    fix: replace jq by grep to check Spoolman update (#482)
    * fix: remove extra space in remove menu
    
    * fix(spoolman): replace jq with grep
    jq isn't included in some minimal installations

--- scripts/spoolman.sh
+++ scripts/spoolman.sh
@@ -248,13 +248,17 @@ function get_spoolman_status() {
 
 function get_local_spoolman_version() {
   local version
-  version=$(jq -r '.version' "${SPOOLMAN_DIR}"/release_info.json)
+  if [[ -d "${SPOOLMAN_DIR}" ]]; then
+    version=$(grep -o '"version":\s*"[^"]*' "${SPOOLMAN_DIR}"/release_info.json | cut -d'"' -f4)
+  else
+    version=""
+  fi
   echo "${version}"
 }
 
 function get_remote_spoolman_version() {
   local version
-  version=$(curl -s "${SPOOLMAN_REPO}" | jq -r '.tag_name')
+  version=$(curl -s "${SPOOLMAN_REPO}" | grep -o '"tag_name":\s*"v[^"]*"' | cut -d'"' -f4)
   echo "${version}"
 }
 
