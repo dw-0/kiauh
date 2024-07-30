2a5f1ed5c7ce44cb210b71832bf6df302aece110
Derpimus - Liam Raithel <76272488+lraithel15133@users.noreply.github.com>
2024-07-30 18:01:45 -0500

    Update beacon.sh

--- scripts/beacon.sh
+++ scripts/beacon.sh
@@ -1,5 +1,16 @@
+#!/usr/bin/env bash
+
 #=======================================================================#
-# Script to Install Beacon                                              #
+# Copyright (C) 2020 - 2024 Dominik Willner <th33xitus@gmail.com>       #
+#                                                                       #
+# This file is part of KIAUH - Klipper Installation And Update Helper   #
+# https://github.com/dw-0/kiauh                                         #
+#                                                                       #
+# This file may be distributed under the terms of the GNU GPLv3 license #
+#=======================================================================#
+
+#=======================================================================#
+# Script to run Beacon Installer by lraithel15133                       #
 # https://github.com/beacon3d/beacon_klipper                            #
 #=======================================================================#
 
