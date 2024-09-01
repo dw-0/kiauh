0a5a48ae72e04948ac261a3c19c64b978ae1b7c1
dw-0 <th33xitus@gmail.com>
2024-09-01 18:39:43 +0200

    Remove simple_config_parser submodule

--- kiauh/core/submodules/simple_config_parser/tests/features/line_type_detection/data/case_line_is_empty.py
+++
@@ -1,9 +0,0 @@
-testcases = [
-    ("", True),
-    (" ", True),
-    ("not empty", False),
-    ("  # indented comment", False),
-    ("not: empty", False),
-    ("also_not= empty", False),
-    ("[definitely_not_empty]", False),
-]
