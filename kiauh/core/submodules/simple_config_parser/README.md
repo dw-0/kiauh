dd1204c044614315b8d18efa9d5854e9baa47437
dw-0 <th33xitus@gmail.com>
2024-09-20 22:09:55 +0200

    chore: remove scp

--- kiauh/core/submodules/simple_config_parser/README.md
+++
@@ -1,6 +0,0 @@
-# Simple Config Parser
-
-A custom config parser inspired by Python's configparser module.
-Specialized for handling Klipper style config files.
-
-
--- kiauh/core/submodules/simple_config_parser/tests/features/line_parsing/data/case_parse_section.py
+++
@@ -1,8 +0,0 @@
-testcases = [
-    ("[test_section]", "test_section"),
-    ("[test_section two]", "test_section two"),
-    ("[section1] # inline comment", "section1"),
-    ("[section2] ; second comment", "section2"),
-    ("[include moonraker-obico-update.cfg]", "include moonraker-obico-update.cfg"),
-    ("[include moonraker_obico_macros.cfg]", "include moonraker_obico_macros.cfg"),
-]
