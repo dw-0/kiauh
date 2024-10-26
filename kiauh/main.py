4925021aa8aa19e20ff40b0f1ec35d5471a3e1d9
dw-0 <th33xitus@gmail.com>
2024-10-26 17:37:48 +0200

    fix: ensure encoding
    Use an alternative approach as in #587 as it introduces an unexpected behavior in printing output
    
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- kiauh/main.py
+++ kiauh/main.py
@@ -6,15 +6,24 @@
 #                                                                         #
 #  This file may be distributed under the terms of the GNU GPLv3 license  #
 # ======================================================================= #
+import io
+import sys
 
 from core.logger import Logger
 from core.menus.main_menu import MainMenu
 from core.settings.kiauh_settings import KiauhSettings
 
 
+def ensure_encoding() -> None:
+    if sys.stdout.encoding == "UTF-8" or not isinstance(sys.stdout, io.TextIOWrapper):
+        return
+    sys.stdout.reconfigure(encoding="utf-8")
+
+
 def main() -> None:
     try:
         KiauhSettings()
+        ensure_encoding()
         MainMenu().run()
     except KeyboardInterrupt:
         Logger.print_ok("\nHappy printing!\n", prefix=False)
