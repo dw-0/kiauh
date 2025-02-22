3bc98eed13b4f944e0365d8d2719b0462843319e
dw-0 <th33xitus@gmail.com>
2025-02-22 16:19:00 +0100

    fix(moonraker): adapt to new moonraker system_dependency.json syntax (#644)
    Signed-off-by: Dominik Willner <th33xitus@gmail.com>

--- kiauh/core/menus/settings_menu.py
+++ kiauh/core/menus/settings_menu.py
@@ -15,7 +15,7 @@ from typing import Literal, Tuple, Type
 from components.klipper import KLIPPER_DIR, KLIPPER_REPO_URL
 from components.klipper.klipper_utils import get_klipper_status
 from components.moonraker import MOONRAKER_DIR, MOONRAKER_REPO_URL
-from components.moonraker.moonraker_utils import get_moonraker_status
+from components.moonraker.utils.utils import get_moonraker_status
 from core.logger import DialogType, Logger
 from core.menus import Option
 from core.menus.base_menu import BaseMenu
