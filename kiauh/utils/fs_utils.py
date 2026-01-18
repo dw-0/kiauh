ba57e922f651df9c2d90bbbae0fce88116082789
Théo Gaillard <theo.gayar@gmail.com>
2026-01-18 23:24:45 +0100

    fix(fs_utils): enhance check_file_exist to follow symlinks and limit search depth

--- kiauh/utils/fs_utils.py
+++ kiauh/utils/fs_utils.py
@@ -24,13 +24,16 @@ from core.logger import Logger
 
 def check_file_exist(file_path: Path, sudo=False) -> bool:
     """
-    Helper function for checking the existence of a file |
+    Helper function for checking the existence of a file.
+    Also works with symlinks (returns False if broken) |
     :param file_path: the absolute path of the file to check
     :param sudo: use sudo if required
     :return: True, if file exists, otherwise False
     """
     if sudo:
-        command = ["sudo", "find", file_path.as_posix()]
+        # -L forces find to follow symlinks
+        # -maxdepth = 0 avoids loosing time if `file_path` is a directory
+        command = ["sudo", "find", "-L", "-maxdepth", "0", file_path.as_posix()]
         try:
             check_output(command, stderr=DEVNULL)
             return True
