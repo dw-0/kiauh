40a43ab3931c394358b09197ead15472a262cf50
moggieuk <32878385+moggieuk@users.noreply.github.com>
2026-08-30 22:08:07 +0700

    initial happy hare extension

---
+++ kiauh/extensions/happy_hare/__init__.py
@@ -0,0 +1,14 @@
+# ======================================================================= #
+#  Copyright (C) 2020 - 2026 Dominik Willner <dev.dw-0@proton.me>         #
+#                                                                         #
+#  This file is part of KIAUH - Klipper Installation And Update Helper    #
+#  https://github.com/dw-0/kiauh                                          #
+#                                                                         #
+#  This file may be distributed under the terms of the GNU GPLv3 license  #
+# ======================================================================= #
+
+from pathlib import Path
+
+HAPPY_HARE_REPO = "https://github.com/moggieuk/Happy-Hare.git"
+HAPPY_HARE_DIR = Path.home().joinpath("Happy-Hare")
+HAPPY_HARE_INSTALL_SCRIPT = HAPPY_HARE_DIR.joinpath("install.sh")
